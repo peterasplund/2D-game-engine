e569d86760132df57fc9e9755904878531dae081
Peter Asplund <peter.asplund@crossroads.se>
2020-01-07 16:29:33 +0100

    fix bug in bg scroll

--- src/bg.h
+++ src/bg.h
@@ -13,19 +13,21 @@ public:
   Bg(std::string path, v2 size, SDL_Renderer* renderer) {
     _texture = AssetManager::Instance(renderer)->getTexture(path);
     _size = size;
-
   }
 
   ~Bg() {
 
   }
 
-  virtual void draw(SDL_Renderer* renderer, float offset) {
+  virtual void draw(SDL_Renderer* renderer, float offset = 0.0f) {
     float localOffset = (int)offset % (int)_size.x;
     SDL_Rect bgRectSrc = { 0, 0, (int)_size.x, (int)_size.y };
     SDL_Rect bgRectDist = { (int)localOffset, 0, (int)_size.x, (int)_size.y };
-    SDL_Rect bgRectDist2 = { (int)_size.x + (int)localOffset, 0, (int)_size.x, (int)_size.y };
     SDL_RenderCopy(renderer, _texture, &bgRectSrc, &bgRectDist);
-    SDL_RenderCopy(renderer, _texture, &bgRectSrc, &bgRectDist2);
+
+    if (offset != 0.0f) {
+      SDL_Rect bgRectDist2 = { (int)_size.x + (int)localOffset, 0, (int)_size.x, (int)_size.y };
+      SDL_RenderCopy(renderer, _texture, &bgRectSrc, &bgRectDist2);
+    }
   }
 };
